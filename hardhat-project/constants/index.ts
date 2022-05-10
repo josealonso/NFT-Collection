c871e1ef16aaae74fac229c9e931254bfced2d2c
josealonso <jose.alonso.programmer@gmail.com>
2022-05-10 20:25:09 +0200

    Comtract deployed on Rinkeby

---
+++ hardhat-project/constants/index.ts
@@ -0,0 +1,3 @@
+export const WHITELIST_CONTRACT_ADDRESS: string = "0xcEF72F827f0c715bEA40C7D04fb831B792adCE42";
+export const METADATA_URL: string = "https://nft-collection-sneh1999.vercel.app/api/";
+
--- hardhat-project/scripts/deploy.ts
+++ hardhat-project/scripts/deploy.ts
@@ -1,30 +1,30 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+import * as Constants from "../constants/index";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  // Address of the whitelisted contract
+  const whitelistContract = Constants.WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Complu NFT
+  const metadataURL = Constants.METADATA_URL;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so NFTContract here is a factory for instances of our NFTCollection contract.
+  */
+  const NFTContract = await ethers.getContractFactory("NFTCollection");
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // deploy the contract
+  const deployedNFTContract = await NFTContract.deploy(metadataURL, whitelistContract);
 
-  await greeter.deployed();
+  // print the address of the deployed contract
+  console.log("NFT Collection Contract Address: ", deployedNFTContract.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+  // await greeter.deployed();
+  // console.log("Greeter deployed to:", greeter.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
